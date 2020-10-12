642cb645cbf43c2334ca2d80aa07816abc071e9f
ha1f <half.ldk.hiz@gmail.com>
2020-10-13 03:22:19 +0900

    Update toolchain

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "GPUImage",
     platforms: [
-        .macOS(.v10_11), .iOS(.v10),
+        .macOS(.v10_12), .iOS(.v10),
     ],
     products: [
         .library(
